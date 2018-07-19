8225bb6085d66e48f7027813b8f10f37453726a0
PabloQuesadaBonilla <pabloquesadabonilla1@gmail.com>
2018-07-18 23:46:30 -0600

    Login Mejorado

---
+++ SistemaReportes/SistemaReportes.DAL/BD.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+
+namespace SistemaReportes.DAL {
+
+
+
+  
+    internal sealed partial class BD
+    {
+
+       
+
+
+        public BD()
+        {
+            
+        }
+
+        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e)
+        {
+            
+        }
+
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+        }
+
+    }
+
+}
+
---
+++ SistemaReportes/SistemaReportes.UI/Clientes.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaReportes.UI
+{
+    public partial class Clientes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBusca_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
